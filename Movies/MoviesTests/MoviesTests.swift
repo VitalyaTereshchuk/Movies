2c2c79e5912481fd0df662701462c1fca2ca989e
Vitalya <vit_less@mail.ru>
2025-11-14 15:32:48 +1000

    Create new project Moviews use SwiftUI

---
+++ Movies/MoviesTests/MoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesTests.swift
+//  MoviesTests
+//
+//  Created by Vitaly on 14.11.2025.
+//
+
+import Testing
+@testable import Movies
+
+struct MoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
